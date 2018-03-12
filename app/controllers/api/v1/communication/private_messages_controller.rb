96503e0103371add2efe4753088f1b731ffbaedf
Амир Хасанов <aworldx@gmail.com>
2018-03-12 21:19:56 +0300

    strong params for api params validation

--- app/controllers/api/v1/communication/private_messages_controller.rb
+++ app/controllers/api/v1/communication/private_messages_controller.rb
@@ -2,17 +2,16 @@ module Api
   module V1
     module Communication
       class PrivateMessagesController < ApplicationController
-        before_action :validate_params
-
-        def validate_params
-          param! :username, String, required: true
-          param! :password, String, required: true
+        def show
+          messages_count = ForumMessagesChecker.new(user_params).call
+          render json: { unread_messages_count: messages_count }, status: :ok
         end
 
-        def show
-          checker = ForumMessagesChecker.new(params)
-          messages_count = checker.call
-          render json: { unread_messages_count: messages_count }, status: :ok
+        private
+
+        def user_params
+          params.require(:username)
+          params.require(:password)
         end
       end
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-  rescue_from RailsParam::Param::InvalidParameterError, with: :render_wrong_params_response
-  rescue_from Selenium::WebDriver::Error::TimeOutError, with: :render_timeout_while_finding_element 
+  rescue_from ActionController::ParameterMissing, with: :render_parameter_missing
+  rescue_from Selenium::WebDriver::Error::TimeOutError, with: :render_timeout_error 
 
-  def render_wrong_params_response(exception)
+  def render_parameter_missing(exception)
     render json: { error: exception.message }, status: :unauthorized
   end
 
-  def render_timeout_while_finding_element(exception)
+  def render_timeout_error(exception)
     render json: { error: exception.message }, status: :not_found
   end
 end
